591903185c8a1669f60f985dc4526ffccebea17d
SamBillingsley <sambillingsleywork@gmail.com>
2023-03-17 03:13:21 -0500

    Complete second problem

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let string = [];
+  if (num < 0) return "ERROR";
+  for (let i = 1; i <= num; i++) {
+    string.push(str);
+  }
+  return string.join("");
 };
 
 // Do not edit below this line
