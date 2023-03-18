c6c1004e5d8f91b28c94517a81dc90c45dc562e3
SamBillingsley <sambillingsleywork@gmail.com>
2023-03-17 20:43:01 -0500

    Finish 4th problem

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...element) {
+  items = element.sort();
+  newArr = [];
+  for (i = 0; i < arr.length; i++) {
+    if (items.includes(arr[i])) {
+      continue;
+    }
+    newArr.push(arr[i]);
+  }
+  return newArr;
 };
 
 // Do not edit below this line
